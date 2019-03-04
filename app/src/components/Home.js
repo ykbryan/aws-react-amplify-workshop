2f0ac428f0f3e0d687a1b42382e54c2848142528
Bryan Chua <ykbryan@hotmail.com>
2019-03-04 16:07:10 +0800

    Moving apps

---
+++ app/src/components/Home.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { API, Analytics, graphqlOperation } from 'aws-amplify';
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    // implement edit function
+    this.getPosts();
+    Analytics.record({ name: 'homeVisit' });
+
+  }
+
+  getPosts = async () => {
+    // TODO: update the graphQL query
+    const graphqlQuery = `query getPosts {
+      listPosts {
+        items {
+          id
+          title
+          message
+        }
+        nextToken
+      } 
+    }`;
+
+    try {
+      const response = await API.graphql(graphqlOperation(graphqlQuery));
+      const { items } = response.data.listPosts;
+
+      if (items.length > 0) {
+        this.setState({
+          posts: items
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    let { posts } = this.state
+    return (
+      <div>
+        <h1>Welcome to Your Home Page.</h1>
+        <div className="card-columns">
+          {posts.map(function (data, index) {
+            return (
+              <div className="card" key={index}>
+                <div className="card-body">
+                  <h5 className="card-title">{data.title}</h5>
+                  <p className="card-text">{data.message}</p>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+export default Home;
---
+++ app/src/components/New.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { API, graphqlOperation } from 'aws-amplify';
+
+class New extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      message: ""
+    }
+  }
+  componentDidMount() {
+    // implement edit function
+  }
+
+  addPost = async (title, message) => {
+    // TODO: update the graphQL mutation
+    const createMutation = `mutation createPost {
+      createPost(input:{
+        title: "${title}"
+        message: "${message}"
+      }) {
+        id
+      }
+    }`;
+
+    try {
+      await API.graphql(graphqlOperation(createMutation));
+      console.log("success, do something here");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  handleSubmit = (event) => {
+    let { title, message } = this.state;
+    this.setState(this.state);
+    this.addPost(title, message);
+    event.preventDefault();
+  }
+
+  handleFieldEdit = (field, event) => {
+    var current = {};
+    current[field] = event.target.value;
+    this.setState(current);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row justify-content-center">
+          <h1>Add New Post</h1>
+          <div className="col-12 text-left">
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <div className="form-group">
+                <label htmlFor="yourTitle">Title</label>
+                <input type="text" ref={(ref) => this.formTitle = ref} className="form-control" id="inputYourTitle" aria-describedby="yourTitleHelp" placeholder="Post Title" onChange={this.handleFieldEdit.bind(this, "title")} />
+                <small id="yourTitleHelp" className="form-text text-muted">What is your blog title?</small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="yourMessage">Message</label>
+                <textarea ref={(ref) => this.formMessage = ref} className="form-control" id="inputMessage" rows="3" placeholder="Your Post Message" onChange={this.handleFieldEdit.bind(this, "message")}></textarea>
+              </div>
+              <div className="d-flex justify-content-around">
+                <button type="reset" className="btn btn-light">Reset</button>
+                <button type="submit" className="btn btn-primary">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default New;
